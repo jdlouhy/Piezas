6ee65d9c71d54a2716a8d0357accb19b73aad515
Jesse Dlouhy <dlouhyjesse@gmail.com>
2020-05-08 11:03:34 -0700

    BugFix

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -14,6 +14,7 @@ class PiezasTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
+//sanity check test
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
@@ -79,7 +80,6 @@ Piezas p;
 TEST(PiezasTest, droppastfull) {
 
 	Piezas p;
-
 	p.dropPiece(0);
 	p.dropPiece(0);
 	p.dropPiece(0);
@@ -96,7 +96,7 @@ Piezas p;
 
 Piece z = p.pieceAt(-1,-1);
 
-ASSERT_EQ(z,Invalid)
+ASSERT_EQ(z,Invalid);
 
 }
 
