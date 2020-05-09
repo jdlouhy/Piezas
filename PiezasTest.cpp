b1fdcbc8e63487257a76fead497342e4d1089d5d
Jesse Dlouhy <dlouhyjesse@gmail.com>
2020-05-08 19:59:56 -0700

    Adding to test

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -163,7 +163,14 @@ TEST(PiezasTest, VerticalScanXWins) {
 
 	Piezas p;
         p.reset();
-	
+     
+        bool check = true;
+ 
+        for (int i = 0; i < BOARD_ROWS; i++){
+        P.dropPiece(0);
+	P.dropPiece(-1);
+	}
+
 	Piece result = p.gameState();
 
 	ASSERT_EQ(result, X);
